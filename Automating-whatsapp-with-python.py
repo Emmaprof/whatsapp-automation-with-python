776abc203f78d237b9a17ab0cf4e7906975a4db0
Emmaprof <emmanuelesin950@gmail.com>
2022-09-09 10:42:47 +0100

    first commit

---
+++ Automating-whatsapp-with-python.py
@@ -0,0 +1,17 @@
+""" SENDING WHATSAPP MESSAGES INSTANTANEOUSLY """
+import pywhatkit  
+pywhatkit.sendwhatmsg_instantly("input your phone number with your country code","i pray you have a peaceful night rest",10)
+
+
+# sending whatsapp messages in  a given hours, minutes and seconds
+import pywhatkit
+pywhatkit.sendwhatmsg("input your phone number with your country code", "message",23,45,7)
+
+
+# sending messages to groups on whatapps using python
+# group id, message and time(time_hours,time_min and time_seconds)
+
+pywhatkit.sendwhatmsg_to_group("group id","msg",20,59,2)
+
+pywhatkit.playont("python") # on youtube
+pywhatkit.search("python") #on goggle
